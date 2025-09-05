34d0e458a13a2da5e0338b9e1e22b0db1781cb81
Vordy <senior.dkflgjyjvfhdsetev@yandex.ru>
2025-09-05 22:26:08 +0300

    Basic abstract window and view classes

---
+++ Agenda.App/BaseView.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia.Controls;
+
+namespace Agenda.App;
+
+public abstract class BaseView : UserControl
+{
+    public Manager Manager { get; set; }
+    public abstract Control LoadUi();
+    
+    public BaseView(Manager manager)
+    {
+        Manager = manager;
+        Content = LoadUi();
+    }
+}
---
+++ Agenda.App/BaseWindow.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+
+namespace Agenda.App;
+
+public abstract class BaseWindow : Window
+{
+    public Manager Manager { get; set; }
+    public PresenterView PresenterView { get; set; }
+    
+    public abstract Control LoadUi();
+    
+    public BaseWindow(Manager manager)
+    {
+        Manager = manager;
+        PresenterView = new PresenterView(manager: Manager);
+        Content = LoadUi();
+    }
+}
