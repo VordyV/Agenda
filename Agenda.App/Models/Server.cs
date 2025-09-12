969115de561cf7c3f42793324c567e0726aafc82
Vordy <senior.dkflgjyjvfhdsetev@yandex.ru>
2025-09-12 20:48:54 +0300

    New model for the server

---
+++ Agenda.App/Models/Server.cs
@@ -0,0 +1,10 @@
+﻿namespace Agenda.App.Models;
+
+public class Server
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Address { get; set; }
+    public int Port { get; set; }
+}
