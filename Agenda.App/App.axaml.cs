9f3e7ad7c9d585dc14afd4b125954f1b6a9b1d7b
Vordy <senior.dkflgjyjvfhdsetev@yandex.ru>
2025-09-20 12:15:35 +0300

    update

--- Agenda.App/App.axaml.cs
+++ Agenda.App/App.axaml.cs
@@ -1,4 +1,6 @@
+using Agenda.App.Drivers;
 using Agenda.App.Models;
+using Agenda.App.Services;
 using Agenda.App.Views;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
@@ -17,7 +19,23 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            Manager manager = new Manager();
+            ConfigService config = new ConfigService("Agenda.db");
+            Manager manager = new Manager(config: config);
+
+            Driver dRconBf2cc = new Driver("RCON BF2CC", () => new Rconbf2cc());
+            dRconBf2cc.AddField("Address", () => new DriverFieldText("address", "Address"));
+            dRconBf2cc.AddField("Port", () => new DriverFieldText("port","Port"));
+            dRconBf2cc.AddField("Password", () => new DriverFieldText("password","Password"));
+            
+            Driver dDaemonBf2cc = new Driver("Daemon BF2CC", () => new Rconbf2cc());
+            dDaemonBf2cc.AddField("Address", () => new DriverFieldText("address", "Address"));
+            dDaemonBf2cc.AddField("Port", () => new DriverFieldText("port","Port"));
+            dDaemonBf2cc.AddField("Username", () => new DriverFieldText("username","Username"));
+            dDaemonBf2cc.AddField("Password", () => new DriverFieldText("password","Password"));
+            
+            manager.AddDriver("rconbf2cc", dRconBf2cc);
+            manager.AddDriver("daemonbf2cc", dDaemonBf2cc);
+            
             desktop.MainWindow = new MainWindowView()
             {
                 DataContext = new MainWindowViewModel(manager: manager)
--- Agenda.App/Manager.cs
+++ Agenda.App/Manager.cs
@@ -1,6 +1,17 @@
-﻿namespace Agenda.App;
+﻿using System.Collections.Generic;
+using Agenda.App.Services;
+
+namespace Agenda.App;
 
 public class Manager
 {
+    public Dictionary<string, Driver> Drivers = new ();
+    public ConfigService Config;
+
+    public Manager(ConfigService config)
+    {
+        Config = config;
+    }
     
+    public void AddDriver(string name, Driver driver) => Drivers.Add(name, driver);
 }
