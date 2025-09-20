d825e3c783f83e67c6e93ec0084fe6f63dd66aef
Vordy <senior.dkflgjyjvfhdsetev@yandex.ru>
2025-09-20 13:49:36 +0300

    New arguments for the AddField method are specified

--- Agenda.App/App.axaml.cs
+++ Agenda.App/App.axaml.cs
@@ -23,15 +23,15 @@ public partial class App : Application
             Manager manager = new Manager(config: config);
 
             Driver dRconBf2cc = new Driver("RCON BF2CC", () => new Rconbf2cc());
-            dRconBf2cc.AddField("Address", () => new DriverFieldText("address", "Address"));
-            dRconBf2cc.AddField("Port", () => new DriverFieldText("port","Port"));
-            dRconBf2cc.AddField("Password", () => new DriverFieldText("password","Password"));
+            dRconBf2cc.AddField("Address", () => new DriverFieldText("address", "Address", minLength: 7, maxLength: 15));
+            dRconBf2cc.AddField("Port", () => new DriverFieldText("port","Port", onlyNumbers: true, maxNumber: 65535));
+            dRconBf2cc.AddField("Password", () => new DriverFieldText("password","Password", minLength: 1, maxLength: 100));
             
             Driver dDaemonBf2cc = new Driver("Daemon BF2CC", () => new Rconbf2cc());
-            dDaemonBf2cc.AddField("Address", () => new DriverFieldText("address", "Address"));
-            dDaemonBf2cc.AddField("Port", () => new DriverFieldText("port","Port"));
-            dDaemonBf2cc.AddField("Username", () => new DriverFieldText("username","Username"));
-            dDaemonBf2cc.AddField("Password", () => new DriverFieldText("password","Password"));
+            dDaemonBf2cc.AddField("Address", () => new DriverFieldText("address", "Address", minLength: 7, maxLength: 15));
+            dDaemonBf2cc.AddField("Port", () => new DriverFieldText("port","Port", onlyNumbers: true, maxNumber: 65535));
+            dDaemonBf2cc.AddField("Username", () => new DriverFieldText("username","Username", minLength: 1, maxLength: 30));
+            dDaemonBf2cc.AddField("Password", () => new DriverFieldText("password","Password", minLength: 1, maxLength: 100));
             
             manager.AddDriver("rconbf2cc", dRconBf2cc);
             manager.AddDriver("daemonbf2cc", dDaemonBf2cc);
